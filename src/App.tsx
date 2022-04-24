2927500f23b64107c0dcf27a25ec4b27b56fe7d8
Hamin Park <haminjp@naver.com>
2022-04-24 16:46:53 +0900

    리스트 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import ListPage from "./pages/ListPage";
+
 const App = () => {
-  return <>App</>;
+  return (
+    <Routes>
+      <Route path="community/list" element={<ListPage />} />
+    </Routes>
+  );
 };
 
 export default App;
