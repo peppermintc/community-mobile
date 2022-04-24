376a34b24de87aab21ec077519b0e2340ce3297f
Hamin Park <haminjp@naver.com>
2022-04-24 19:16:30 +0900

    리스트 페이지 제목 추가

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+
+const Page = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+`;
+
+const Title = styled.span`
+  font-weight: 700;
+  font-size: 22px;
+  margin: 34px 0 20px 30px;
+  color: #222222;
+`;
+
 const ListPage: React.FC = () => {
-  return <div>ListPage</div>;
+  return (
+    <Page>
+      <Title>커뮤니티</Title>
+    </Page>
+  );
 };
 
 export default ListPage;
