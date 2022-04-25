7b8148c8ce1ea1261c214d4eb8636751c3e87e4e
Hamin Park <haminjp@naver.com>
2022-04-25 15:41:13 +0900

    리스트 페이지 스타일 업데이트

--- src/pages/ListPage.tsx
+++ src/pages/ListPage.tsx
@@ -4,12 +4,13 @@ const Page = styled.div`
   display: flex;
   flex-direction: column;
   height: 100vh;
+  width: 100%;
 `;
 
 const Title = styled.span`
   font-weight: 700;
   font-size: 22px;
-  margin: 34px 0 20px 30px;
+  margin: 34px auto 20px 30px;
   color: #222222;
 `;
 
