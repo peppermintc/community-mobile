ddfcef6c8f06210c4250b814f8783ade9003827b
Hamin Park <haminjp@naver.com>
2022-04-28 02:36:45 +0900

    앱 시작시 리스트 페이지로 이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useLayoutEffect } from "react";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import useActionCreators from "./hooks/useActionCreators";
 import DetailPage from "./pages/DetailPage";
 import ListPage from "./pages/ListPage";
@@ -14,6 +14,7 @@ const App = () => {
 
   return (
     <Routes>
+      <Route path="/" element={<Navigate replace to="/community/list" />} />
       <Route path="community/list" element={<ListPage />} />
       <Route path="community/post/:post_pk" element={<DetailPage />} />
       <Route path="community/post/new" element={<WritePage />} />
