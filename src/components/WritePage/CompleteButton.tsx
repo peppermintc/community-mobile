50a1323fc07a242815b5684c8e6fe90e3fde23d4
Hamin Park <haminjp@naver.com>
2022-04-28 01:05:03 +0900

    폼 작성 완료 버튼 활성화 기능 추가

--- src/components/WritePage/CompleteButton.tsx
+++ src/components/WritePage/CompleteButton.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useLocation, useNavigate } from "react-router-dom";
 import styled from "styled-components";
@@ -11,7 +12,6 @@ const CompleteButtonContainer = styled.button`
   border: none;
   height: 36px;
   width: 64px;
-  background-color: #2c7fff;
   border-radius: 8px;
   color: #ffffff;
   font-size: 14px;
@@ -20,11 +20,14 @@ const CompleteButtonContainer = styled.button`
   justify-content: center;
   align-items: center;
   cursor: pointer;
+  background-color: ${(props) => (props.disabled ? "#ebebeb" : "#2c7fff")};
 `;
 
 const CompleteButton = () => {
   const location = useLocation();
   const navigate = useNavigate();
+
+  const [isDisabled, setIsDisabled] = useState(true);
 
   const form = useSelector((state: RootState) => state.community.form);
   const posts = useSelector((state: RootState) => state.community.posts);
@@ -49,8 +52,16 @@ const CompleteButton = () => {
     navigate("/community/list", { state: prevScrollPosition });
   };
 
+  useEffect(() => {
+    if (!form) return;
+
+    const isFormFilled = form.title !== "" && form.content !== "";
+    if (isFormFilled) setIsDisabled(false);
+    else setIsDisabled(true);
+  }, [form]);
+
   return (
-    <CompleteButtonContainer onClick={onButtonClick}>
+    <CompleteButtonContainer onClick={onButtonClick} disabled={isDisabled}>
       완료
     </CompleteButtonContainer>
   );
